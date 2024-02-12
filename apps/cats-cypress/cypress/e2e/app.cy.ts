09d3a082866e2a77f344753e533bdcc22c6b4a6d
Emily Xiong <xiongemi@gmail.com>
2024-02-12 15:21:25 -0500

    add cat cypress testing files

---
+++ apps/cats-cypress/cypress/e2e/app.cy.ts
@@ -0,0 +1,23 @@
+import { getGreeting } from '../support/app.po';
+
+describe('Should display a cat fact', () => {
+  beforeEach(() => {
+    cy.intercept('https://catfact.ninja/fact', {
+      fixture: 'cat-fact-response.json',
+    }).as('getCatsFact');
+    cy.visit('/');
+    cy.wait(['@getCatsFact']);
+  });
+
+  it('should display title', () => {
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains(/Cat Facts/);
+  });
+
+  it('should display a cat fact', () => {
+    cy.get('[data-testid="carousel-card-content"]').should('exist');
+    cy.get('[data-testid="carousel-card-content"]').contains(
+      'A cat\u2019s heart beats nearly twice as fast as a human heart, at 110 to 140 beats a minute.'
+    );
+  });
+});
