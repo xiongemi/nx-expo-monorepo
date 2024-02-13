bb3d012b322570dcbf0ad291142655063154b230
Emily Xiong <xiongemi@gmail.com>
2024-02-13 18:22:52 -0500

    fix ui test

--- libs/ui/jest.config.ts
+++ libs/ui/jest.config.ts
@@ -10,4 +10,15 @@ module.exports = {
   moduleNameMapper: {
     '.svg': '@nx/expo/plugins/jest/svg-mock',
   },
+  transform: {
+    '.[jt]sx?$': [
+      'babel-jest',
+      {
+        configFile: __dirname + '/.babelrc',
+      },
+    ],
+    '^.+.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp|ttf|otf|m4v|mov|mp4|mpeg|mpg|webm|aac|aiff|caf|m4a|mp3|wav|html|pdf|obj)$': require.resolve(
+      'jest-expo/src/preset/assetFileTransformer.js'
+    ),
+  },
 };
