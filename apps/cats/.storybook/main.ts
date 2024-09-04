93cd55ee14ccd59bb6cc89f222b12c69e52b7ca3
Emily Xiong <xiongemi@gmail.com>
2024-09-04 18:23:49 -0400

    fix storybook for cats

--- apps/cats/.storybook/main.ts
+++ apps/cats/.storybook/main.ts
@@ -8,14 +8,46 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@nx/react/plugins/storybook',
-    '@storybook/addon-react-native-web',
-    '@chromatic-com/storybook'
+    '@chromatic-com/storybook',
+    // '@storybook/addon-react-native-web'
   ],
 
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
   },
+
+ webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'react-native$': 'react-native-web',
+      };
+      config.resolve.extensions = [
+        '.web.tsx',
+        '.web.ts',
+        '.web.jsx',
+        '.web.js',
+        ...(config.resolve.extensions ?? []),
+      ];
+      config.module ??= {};
+      config.module.rules ??= [];
+      config.module.rules.push({
+        test: /\.(js|jsx)$/,
+        include: /react-native-vector-icons/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            ['@babel/preset-react', { runtime: 'automatic' }],
+          ],
+        },
+      });
+
+    }
+    return config;
+  },
+
 
   docs: {},
 
